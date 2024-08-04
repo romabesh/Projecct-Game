0a59319d61013d052a68ea1cad4750140c1c0203
Roman Beshulia <gnom9944@gmail.com>
2024-08-04 22:45:06 +0300

    Create Project

---
+++ Source/Project/InventorySystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+
+class PROJECT_API InventorySystem : public UObject
+{
+public:
+	InventorySystem();
+	~InventorySystem();
+};
